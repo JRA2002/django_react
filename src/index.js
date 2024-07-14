f268f97be7e7a803c25437351b9f00fd8d46f3a5
javier <javier_2790@outlook.com>
2024-07-14 23:46:21 +0200

    feat: add hello.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import REACT from 'react';
 import ReactDOM from 'react-dom/client';
+import Hello from './Hello';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-function Hello() {
-    return <div>
-        <h1>Hello World</h1>
-    </div>
-}
+
 root.render(
-    <><Hello /><Hello /><Hello /><Hello /></>
+    <>
+    <Hello />
+    <Hello />
+    <Hello />
+    <Hello />
+    </>
     
     )
